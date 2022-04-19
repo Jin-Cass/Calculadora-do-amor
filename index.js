495132b5dca9ffa18334fe52fbc9495a64b06e12
Cass <douglaszatara123@gmail.com>
2022-04-19 20:16:01 -0300

    update

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 function ValentineDay(){
 
 
-var numeroAleatorio = Math.random ();
-numeroAleatorio = numeroAleatorio * 100;
-numeroAleatorio = Math.floor(numeroAleatorio) + 1;
-var resposta = document.getElementById("resposta");
-resposta.innerHTML = "Your chances are : " + numeroAleatorio + "%.";
-}
+    var numeroAleatorio = Math.random ();
+    numeroAleatorio = numeroAleatorio * 100;
+    numeroAleatorio = Math.floor(numeroAleatorio) + 1;
+    // exemplo de condicionais //
+
+    if (numeroAleatorio >70){numeroAleatorio =  "you are a wonderful couple " + numeroAleatorio ;
+    }
+    else{(numeroAleatorio = "Maybe it works " + numeroAleatorio );
+    }
+    
+    
+    var resposta = document.getElementById("resposta");
+    resposta.innerHTML = numeroAleatorio + " %";
+    }
+    
+    
